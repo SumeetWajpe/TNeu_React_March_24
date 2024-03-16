677c2f7ebb439b87b5ef1b9e1b4c67a781eed0a4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-16 12:56:49 +0530

    ✨ Using map for iterating

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -4,17 +4,35 @@ import React from "react";
 import Message from "./message/message.component";
 
 class App extends React.Component {
-  firstMessage = {
-    msg: "Hello",
-    from: "Jim",
-    to: "Tim",
-    imageUrl:
-      "https://img.freepik.com/free-vector/v813-aew-05_53876-166405.jpg",
-  };
+  messages = [
+    {
+      msg: "Hello",
+      from: "Jim",
+      to: "Tim",
+      imageUrl:
+        "https://img.freepik.com/free-vector/v813-aew-05_53876-166405.jpg",
+    },
+    {
+      msg: "Hey",
+      from: "John",
+      to: "Carter",
+      imageUrl:
+        "https://t4.ftcdn.net/jpg/02/19/68/97/360_F_219689742_N53gBb5bVYPzWTOaps8A2LAUx62jbvWo.jpg",
+    },
+    {
+      msg: "Hola",
+      from: "Peter",
+      to: "Parker",
+      imageUrl:
+        "https://www.shutterstock.com/image-vector/hola-word-lettering-spanish-hello-260nw-2170222797.jpg",
+    },
+  ];
   render() {
     return (
       <div>
-        <Message messageDetails={this.firstMessage} />
+        {this.messages.map(m => (
+          <Message messageDetails={m} />
+        ))}
 
         {/* <Message
           msg="Hey"
