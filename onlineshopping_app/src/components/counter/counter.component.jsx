e6b8f2f36f32de527876894b2f500f1dbf97bb85
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-03-23 12:24:44 +0530

    ✨ using useState() Hook with objects

--- onlineshopping_app/src/components/counter/counter.component.jsx
+++ onlineshopping_app/src/components/counter/counter.component.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
 export function Counter() {
-  const [count, setCount] = useState(0);
-  const [age, setAge] = useState(18);
+  const [data, setData] = useState({ count: 0, age: 18 });
   return (
     <>
-      <p>Count : {count} </p>
-      <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+      <p>Count : {data.count} </p>
+      <button
+        className="btn btn-primary"
+        onClick={() => setData({ ...data, count: data.count + 1 })}
+      >
         ++
       </button>
       <hr />
-      <p>Age : {age}</p>
-      <button className="btn btn-primary" onClick={() => setAge(age + 1)}>
+      <p>Age : {data.age}</p>
+      <button
+        className="btn btn-primary"
+        onClick={() => setData({ ...data, age: data.age + 10 })}
+      >
         ++
       </button>
     </>
